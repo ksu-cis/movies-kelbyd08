fe85afbb9596797a401dde9b002a7da9698c12d0
Kelby Dinkel <kdinkel08@outlook.com>
2019-11-18 16:38:59 -0600

    in class work

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -27,5 +27,43 @@ namespace Movies
         }
 
         public List<Movie> All { get { return movies; } }
+
+        public List<Movie> Search(string searchString)
+        {
+            if (searchString == null) return All;
+            List<Movie> result = new List<Movie>();
+            foreach(Movie movie in movies)
+            {
+                if(movie.Title.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+                {
+                    result.Add(movie);
+                }
+            }
+            return result;
+        }
+        public List<Movie> Filter(List<string> ratings)
+        {
+            List<Movie> result = new List<Movie>();
+            foreach(Movie movie in movies)
+            {
+                if (ratings.Contains(movie.MPAA_Rating))
+                {
+                    result.Add(movie);
+                }
+            }
+            return result;
+        }
+        public List<Movie> SearchAndFilter(string search, List<string> ratings)
+        {
+            List<Movie> result = new List<Movie>();
+
+            foreach(Movie movie in movies)
+            {
+                if (ratings.Contains(movie.MPAA_Rating) && movie.Title.Contains(search, StringComparison.OrdinalIgnoreCase))
+                    result.Add(movie);
+            }
+
+            return result;
+        }
     }
 }
